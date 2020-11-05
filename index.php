4592d082112abc78a91891a2e380efc85408dfa4
429273-16 <429273-16>
2020-11-05 09:56:56 +0300

    lab4

--- index.php
+++ index.php
@@ -13,3 +13,29 @@ include "Povarnitcina/Solve.php";
 include "Povarnitcina/Ksolve.php";
 include "Povarnitcina/MyLog.php";
 
+
+ini_set("display_errors", 1);
+error_reporting(-1);
+
+try {
+    $b = new Ksolve();
+    $values = array();
+
+    for ($i = 1; $i < 4; $i++) {
+        echo "Введите " . $i . " аргумент: ";
+        $values[] = readline();
+    }
+    $va = $values[0];
+    $vb = $values[1];
+    $vc = $values[2];
+
+    MyLog::log("Введено уравнение " . $va . "x^2 + " . $vb . "x + " . $vc . " = 0");
+    $x = $b->solve($va, $vb, $vc);
+
+    $str = implode(", ", $x);
+    MyLog::log("Корни уравнения: " . $str);
+} catch (PovarExeption $e) {
+    MyLog::log($e->getMessage());
+}
+
+MyLog::write();
